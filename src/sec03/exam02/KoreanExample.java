2e72db909b7f4ad260694b5fd4ef69a89916d90a
pky0704 <ghghgh0704@naver.com>
2021-07-30 17:30:59 +0900

    생성자에서 필드 초기화, 객체 생성 후 필드값 출력

---
+++ src/sec03/exam02/KoreanExample.java
@@ -0,0 +1,15 @@
+package sec03.exam02;
+//객체 생성 후 필드값 출력 
+public class KoreanExample {
+
+	public static void main(String[] args) {
+		Korean k1 = new Korean("박세리", "01234-12343");
+		System.out.println("k1.name : "+k1.name);
+		System.out.println("k1.ssn"+k1.ssn);
+		
+		Korean k2 = new Korean("김세희", "98445-23423");
+		System.out.println("k2.name"+k2.name);
+		System.out.println("k2.ssn"+k2.ssn);
+	}
+
+}
