82e98702cb257cfa3f48cec706ec14baf83e9684
pky0704 <ghghgh0704@naver.com>
2021-07-31 17:01:55 +0900

    클래스의 접근 제한

---
+++ src/sec06/exam03/package1/A.java
@@ -0,0 +1,6 @@
+package sec06.exam03.package1;
+
+//default 접근 제한  : 같은 패키지 내에서 제한없이 사용
+class A {
+
+}
---
+++ src/sec06/exam03/package1/B.java
@@ -0,0 +1,5 @@
+package sec06.exam03.package1;
+
+public class B {
+	A a; //같은 패키지 내 A클래스 접근 가능
+}
