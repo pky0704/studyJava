7f6c498048923057e1ed6f4210105202d8f47514
pky0704 <ghghgh0704@naver.com>
2021-08-02 11:29:36 +0900

    클래스 상속

---
+++ src/chap07/sec01/exam01/CellPhone.java
@@ -0,0 +1,16 @@
+package chap07.sec01.exam01;
+//상속
+//부모클래스
+public class CellPhone {
+	//필드
+	String model;
+	String color;
+	
+	//매소드
+	void powerOn() {System.out.println("전원을 켭니다.");}
+	void powerOff() {System.out.println("전원을 끕니다.");}
+	void bell() {System.out.println("벨이 울립니다.");}
+	void sendVoice(String message) {System.out.println("자기 : " + message);}
+	void recieve(String message) {System.out.println("상대방 : " + message);}
+	void hangUp() {System.out.println("전화를 끊습니다.");}
+}
